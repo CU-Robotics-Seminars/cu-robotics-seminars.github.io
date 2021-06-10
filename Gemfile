defb6c67dc7324066d76fdb71f79f4559f639e6a
Joewie J. Koh <joewie@eigenspectra.com>
2021-06-10 14:39:04 -0600

    Commit website code

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'https://rubygems.org'
+group :jekyll_plugins do
+    gem 'jekyll'
+    gem 'jekyll-email-protect'
+    gem 'jekyll-github-metadata'
+    gem 'jekyll-paginate-v2'
+    gem 'jekyll-redirect-from'
+    gem 'jekyll-scholar'
+    gem 'jekyll-twitter-plugin'
+    gem 'jemoji'
+    gem 'kramdown-parser-gfm'
+    gem 'unicode_utils'
+    gem 'webrick'
+end
---
+++ assets/js/common.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('a.abstract').click(function() {
+        $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
+    });
+    $('a.bio').click(function() {
+        $(this).parent().parent().find(".bio.hidden").toggleClass('open');
+    });
+    $('a.bibtex').click(function() {
+        $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
+    });
+    $('.navbar-nav').find('a').removeClass('waves-effect waves-light');
+});
